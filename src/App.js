aed6a1f3f691345cf1b55d162880f4dabb4bad26
Mohamed Diallo <mohameddiallo.msd11@gmail.com>
2023-09-18 18:17:17 +0000

    push2

--- src/App.js
+++ src/App.js
@@ -4,19 +4,16 @@ import './App.css';
 // import Accueil from "./Components/Dashdoard/Accueil/Accueil";
 // import Footer from "./Components/Dashdoard/Footer/Footer";
 // import Sidebar from "./Components/Sidebar/Sidebar";
-import Produit from "./Components/Produit/Produit";
-import HommesProduct from "./Components/HommesProduct/HommesProduct";
-import Acueilparcour from "./Components/ParcoursClient/Acueilparcour/Acueilparcour";
-import Pageaccueil from "./Components/Dashdoard/Pageaccueil/Pageaccueil";
+// import Produit from "./Components/Produit/Produit";
+// import HommesProduct from "./Components/HommesProduct/HommesProduct";
+// import Acueilparcour from "./Components/ParcoursClient/Acueilparcour/Acueilparcour";
+// import Pageaccueil from "./Components/Dashdoard/Pageaccueil/Pageaccueil";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Monpanier from "./Components/ParcoursClient/Monpanier/Monpanier";
-import Infoclient from "./Components/ParcoursClient/InfoClientValidation/InfoClient/Infoclient";
-import Modelivraison from "./Components/ParcoursClient/ModeLivraison/Modelivraison";
-import Modepaiement from "./Components/ParcoursClient/Modepaiement/Modepaiement";
-import Compte from "./Components/Compteuser/Compte/Compte";
-import Commande from './Components/Compteuser/Comande/Commande'
-import MonPanier from "./Components/Compteuser/MonPanier/MonPanier";
-import Favoris from './Components/Compteuser/Favoris/Favoris'
+// import Monpanier from "./Components/ParcoursClient/Monpanier/Monpanier";
+// import Infoclient from "./Components/ParcoursClient/InfoClientValidation/InfoClient/Infoclient";
+// import Modelivraison from "./Components/ParcoursClient/ModeLivraison/Modelivraison";
+// import Modepaiement from "./Components/ParcoursClient/Modepaiement/Modepaiement";
+import Home from "./Pages/Home/Home";
 
 function App() {
   return (
@@ -34,22 +31,19 @@ function App() {
 
 
       <Router>
-      <Routes>
-        <Route path="/" index element={<Pageaccueil />}></Route>
-        <Route path="/produit" element={<Produit />} />
-        <Route path='/hommeProduit' element={<HommesProduct />} />
-        <Route exact path='/detailProduit'  element={<Acueilparcour />}></Route>
-        <Route path="/monPanier" element={<Monpanier />} />
-        <Route path="/infopersonnelle" element={<Infoclient />} />
-        <Route path="/modelivraison" element={<Modelivraison />} />
-        <Route path="/modepaiement" element={<Modepaiement />} />
-        <Route path='/compte' element={<Compte />} />
-        <Route path='/commande' element={<Commande />} />
-        <Route path='/panier' element={<MonPanier />} />
-        <Route path='/favoris' element={<Favoris />} />
-      </Routes>
-    </Router>
-      
+        <Routes>
+          <Route path="" />
+          <Route path="/" index element={<Home />} />
+          {/* <Route path="/produit" element={<Produit />} />
+          <Route path='/hommeProduit' element={<HommesProduct />} />
+          <Route exact path='/detailProduit' element={<Acueilparcour />}></Route>
+          <Route path="/monPanier" element={<Monpanier />} />
+          <Route path="/infopersonnelle" element={<Infoclient />} />
+          <Route path="/modelivraison" element={<Modelivraison />} />
+          <Route path="/modepaiement" element={<Modepaiement />} /> */}
+        </Routes>
+      </Router>
+
 
     </div>
   );
